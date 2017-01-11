4c88ccf3f447873cc5929f381796b9bc47fcc876
Marty Thierry <marty@martylouis.com>
2017-01-11 14:38:25 -0600

    Update blog/index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,33 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router'
-import sortBy from 'lodash/sortBy'
 import { prefixLink } from 'gatsby-helpers'
 import Helmet from "react-helmet"
-import access from 'safe-access'
 import { config } from 'config'
-import include from 'underscore.string/include'
+import PostList from '../../components/PostList'
 
 class PostIndex extends React.Component {
-  render () {
-
-    const postLinks = []
-      // Sort pages.
-      const sortedPosts = sortBy(this.props.route.pages, (post) =>
-        access(post, 'data.date')
-      ).reverse()
-
-      sortedPosts.forEach((post) => {
-        // console.log(post)
-        // Posts are those with md extension that are not 404 posts OR have a date (meaning they're a react component post).
-        if (access(post, 'file.ext') === 'md' && access(post, 'file.dir') === 'blog' && !include(post.path, '/404') || access(post, 'data.date')) {
-          const title = access(post, 'data.title') || post.path
-          postLinks.push(
-            <li key={post.path}>
-              <Link to={prefixLink(post.path)}>{title}</Link>
-            </li>
-          )
-        }
-      })
+  render() {
     return (
       <div>
         <Helmet
@@ -40,16 +19,10 @@ class PostIndex extends React.Component {
         <h1>
           This is Blog index!
         </h1>
-        <ul className="post-list">
-          {postLinks}
-        </ul>
+        <PostList postRoute={this.props.route} postDir='blog' />
       </div>
     )
   }
-}
-
-PostIndex.propTypes = {
-  route: React.PropTypes.object
 }
 
 export default PostIndex
